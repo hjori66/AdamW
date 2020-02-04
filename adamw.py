59082522b75d9ea94c39ff3bf7116951701e8846
hjori66 <ghks0830@kaist.ac.kr>
2020-02-04 12:43:43 +0900

    Reference : 2019 Summer Freshman Seminar

--- adamw.py
+++ adamw.py
@@ -5,9 +5,9 @@ from torch.optim import Optimizer
 
 class AdamW(Optimizer):
     def __init__(self, params, lr=1e-3, beta1=0.9, beta2=0.99, eps=1e-7,
-                 amsgrad=False, weight_decay=0, decoupled_weight_decay=0):
+                 amsgrad=False, weight_decay=0):
         defaults = dict(lr=lr, beta1=beta1, beta2=beta2, eps=eps, amsgrad=amsgrad,
-                        weight_decay=weight_decay, decoupled_weight_decay=decoupled_weight_decay)
+                        weight_decay=weight_decay)
         super(AdamW, self).__init__(params, defaults)
 
     def step(self):
@@ -35,8 +35,8 @@ class AdamW(Optimizer):
                 b2 = group['beta2']
                 t += 1
 
-                if group['weight_decay'] != 0:
-                    grad.add_(group['weight_decay'], p.data)  # weight decay
+                # if group['weight_decay'] != 0:
+                #     grad.add_(group['weight_decay'], p.data)  # weight decay
 
                 m = torch.mul(m, b1) + (1-b1) * grad
                 v = torch.mul(v, b2) + (1-b2) * grad**2
@@ -54,7 +54,7 @@ class AdamW(Optimizer):
                 state['m'] = m
                 state['v'] = v
 
-                if group['decoupled_weight_decay'] != 0:
-                    grad.add_(group['decoupled_weight_decay'], p.data)  # decoupled weight decay, AdamW
+                if group['weight_decay'] != 0:
+                    grad.add_(group['weight_decay'], p.data)  # decoupled weight decay, AdamW
 
         return loss
--- neural_network_experiment.py
+++ neural_network_experiment.py
@@ -15,6 +15,8 @@ def main(args, model, optimizer, trainloader, testloader):
         running_loss = 0
         for i, data in enumerate(trainloader, 0):
             x, y = data
+            x = x.cuda()
+            y = y.cuda()
             optimizer.zero_grad()
             outputs = model(x)
             loss = criterion(outputs, y)
@@ -28,6 +30,8 @@ def main(args, model, optimizer, trainloader, testloader):
     with torch.no_grad():
         for idx, data in enumerate(testloader):
             x, y = data
+            x = x.cuda()
+            y = y.cuda()
             outputs = model(x)
             _, predicted = torch.max(outputs.data, 1)
             loss = criterion(outputs, y)
@@ -39,48 +43,63 @@ def main(args, model, optimizer, trainloader, testloader):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--lr', default=1e-3, type=float)
-    parser.add_argument('--batch_size', default=128, type=float)
-    parser.add_argument('--epochs', default=2000000, type=int)
+    parser.add_argument('--batch_size', default=64, type=float)
+    parser.add_argument('--epochs', default=120, type=int)
     parser.add_argument('--eps', default=1e-7, type=float)
     parser.add_argument('--beta1', default=0.9, type=float)
     parser.add_argument('--beta2', default=0.99, type=float)
     parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--weight_decay', default=1e-3, type=float)
     args = parser.parse_args()
     fix_seed(args.seed)
 
     trainloader, testloader = dataset(args.batch_size)
-    model = CNN()
+    # model = CNN()
+    # model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet18', pretrained=True)
+    # model = torch.hub.load('pytorch/vision:v0.5.0', 'vgg11')
+    # model.cuda()
+    # print('Processing AMSGrad...')
+    # amsgrad = AMSGrad(model.parameters(), lr=args.lr,
+    #                   beta1=args.beta1, beta2=args.beta2, eps=args.eps)
+    # ams_train_loss, ams_test_loss = main(args, model, amsgrad, trainloader, testloader)
 
-    print('Processing AMSGrad...')
-    amsgrad = AMSGrad(model.parameters(), lr=args.lr,
-                      beta1=args.beta1, beta2=args.beta2, eps=args.eps)
-    ams_train_loss, ams_test_loss = main(args, model, amsgrad, trainloader, testloader)
+    model = torch.hub.load('pytorch/vision:v0.5.0', 'vgg11')
+    model.cuda()
+    print('\nProcessing AdamW (with decoupled weight decay)...')
+    adamw = torch.optim.AdamW(model.parameters(), lr=args.lr,
+                              betas=(args.beta1, args.beta2), eps=args.eps, weight_decay=args.weight_decay)
+    adamw_train_loss, adamw_test_loss = main(args, model, adamw, trainloader, testloader)
 
+    model = torch.hub.load('pytorch/vision:v0.5.0', 'vgg11')
+    model.cuda()
     print('\nProcessing SGD...')
     sgd = torch.optim.SGD(model.parameters(), lr=args.lr)
     sgd_train_loss, sgd_test_loss = main(args, model, sgd, trainloader, testloader)
 
+    model = torch.hub.load('pytorch/vision:v0.5.0', 'vgg11')
+    model.cuda()
     print('\nProcessing Adam...')
     adam = torch.optim.Adam(model.parameters(), lr=args.lr,
                             betas=(args.beta1, args.beta2), eps=args.eps)
     adam_train_loss, adam_test_loss = main(args, model, adam, trainloader, testloader)
 
+    model = torch.hub.load('pytorch/vision:v0.5.0', 'vgg11')
+    model.cuda()
     print('\nProcessing Adam with weight decay...')
     adam_wd = torch.optim.Adam(model.parameters(), lr=args.lr,
-                            betas=(args.beta1, args.beta2), eps=args.eps, weight_decay=1e-3)
+                            betas=(args.beta1, args.beta2), eps=args.eps, weight_decay=args.weight_decay)
     adam_wd_train_loss, adam_wd_test_loss = main(args, model, adam_wd, trainloader, testloader)
 
-    print('\nProcessing AdaGrad...')
-    adagrad = torch.optim.Adagrad(model.parameters())
-    ada_train_loss, ada_test_loss = main(args, model, adagrad, trainloader, testloader)
+    # model = torch.hub.load('pytorch/vision:v0.5.0', 'vgg11')
+    # model.cuda()
+    # print('\nProcessing AdaGrad...')
+    # adagrad = torch.optim.Adagrad(model.parameters())
+    # ada_train_loss, ada_test_loss = main(args, model, adagrad, trainloader, testloader)
 
-    print('\nProcessing AdamW (with decoupled weight decay)...')
-    adamw = torch.optim.AdamW(model.parameters(), lr=args.lr,
-                              betas=(args.beta1, args.beta2), eps=args.eps, decoupled_weight_decay=1e-3)
-    adamw_train_loss, adamw_test_loss = main(args, model, adamw, trainloader, testloader)
-
-    train_loss_list = [sgd_train_loss, adam_train_loss, adam_wd_train_loss, ams_train_loss, ada_train_loss, adamw_train_loss]
-    test_loss_list = [sgd_test_loss, adam_test_loss, adam_wd_test_loss, ams_test_loss, ada_test_loss, adamw_test_loss]
-    opt_list = ['SGD', 'Adam', 'Adam_weight_decay', 'AMSGrad', 'AdaGrad', 'AdamW']
+    train_loss_list = [sgd_train_loss, adam_train_loss, adam_wd_train_loss, adamw_train_loss]
+    test_loss_list = [sgd_test_loss, adam_test_loss, adam_wd_test_loss, adamw_test_loss]
+    # train_loss_list = [sgd_train_loss, adam_train_loss, adam_wd_train_loss, ams_train_loss, ada_train_loss, adamw_train_loss]
+    # test_loss_list = [sgd_test_loss, adam_test_loss, adam_wd_test_loss, ams_test_loss, ada_test_loss, adamw_test_loss]
+    opt_list = ['SGD', 'Adam', 'Adam_weight_decay', 'AdamW']
 
     plot_CIFAR(train_loss_list, test_loss_list, opt_list, args.epochs)

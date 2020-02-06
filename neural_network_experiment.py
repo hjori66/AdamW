99047f4c9e11760edbe1e901b18f2592e46d4837
hjori66 <ghks0830@kaist.ac.kr>
2020-02-06 11:13:57 +0900

    Reference : 2019 Summer Freshman Seminar

--- neural_network_experiment.py
+++ neural_network_experiment.py
@@ -1,7 +1,7 @@
 from model import CNN
 from amsgrad import AMSGrad
 from adamw import AdamW
-from cifar_dataset import cifar100_dataset
+from cifar_dataset import cifar10_dataset
 from utils import fix_seed, plot_CIFAR
 import torch
 import argparse
@@ -38,7 +38,7 @@ def main(args, model, optimizer, trainloader, testloader):
                 _, predicted = torch.max(outputs.data, 1)
                 loss = criterion(outputs, y)
                 total_test_loss += loss.item()
-            test_loss.append(total_test_loss // (idx+1))
+            test_loss.append(total_test_loss / (idx+1))
 
     with open('results/loss/' + args.optimizer + '_train_loss.txt', 'wb') as f:
         pickle.dump(train_loss, f)
@@ -51,27 +51,27 @@ def main(args, model, optimizer, trainloader, testloader):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--lr', default=1e-3, type=float)
+    parser.add_argument('--lr', default=1e-4, type=float)
     parser.add_argument('--batch_size', default=128, type=float)
-    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('--epochs', default=20, type=int)
     parser.add_argument('--momentum', default=0.9, type=float)
     parser.add_argument('--eps', default=1e-7, type=float)
     parser.add_argument('--beta1', default=0.9, type=float)
     parser.add_argument('--beta2', default=0.99, type=float)
     parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--weight_decay', default=1e-3, type=float)
-    parser.add_argument('--optimizer', default='SGD', type=str)
+    parser.add_argument('--weight_decay', default=1e-2, type=float)
+    parser.add_argument('--optimizer', default='draw_plot', type=str)
     args = parser.parse_args()
     fix_seed(args.seed)
 
-    trainloader, testloader = cifar100_dataset(args.batch_size)
+    trainloader, testloader = cifar10_dataset(args.batch_size)
 
-    model = torch.hub.load('pytorch/vision:v0.5.0', 'vgg11')
+    model = torch.hub.load('pytorch/vision:v0.5.0', 'vgg11', pretrained=True)
     model.cuda()
 
     if args.optimizer == 'SGD':
         print('\nProcessing SGD...')
-        sgd = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+        sgd = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=args.momentum)
         sgd_train_loss, sgd_test_loss = main(args, model, sgd, trainloader, testloader)
 
     elif args.optimizer == 'Adam':
@@ -89,7 +89,7 @@ if __name__ == '__main__':
     elif args.optimizer == 'AdamW':
         print('\nProcessing AdamW (with decoupled weight decay)...')
         adamw = torch.optim.AdamW(model.parameters(), lr=args.lr,
-                                  betas=(args.beta1, args.beta2), eps=args.eps, weight_decay=args.weight_decay)
+                                  betas=(args.beta1, args.beta2), eps=args.eps, weight_decay=1.0)
         adamw_train_loss, adamw_test_loss = main(args, model, adamw, trainloader, testloader)
 
     elif args.optimizer == 'AMSGrad':
@@ -104,7 +104,8 @@ if __name__ == '__main__':
         ada_train_loss, ada_test_loss = main(args, model, adagrad, trainloader, testloader)
 
     else:
-        opt_list = ['SGD', 'Adam', 'Adam_weight_decay', 'AdamW', 'AMSGrad', 'AdaGrad']
+        # opt_list = ['SGD', 'Adam', 'Adam_weight_decay', 'AdamW', 'AMSGrad', 'AdaGrad']
+        opt_list = ['Adam']
         train_loss_list = []
         test_loss_list = []
         for opt in opt_list:
